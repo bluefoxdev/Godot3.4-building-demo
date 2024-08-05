8a7385dbedaf6925d4f7521504d2a7c0c97f233e
bluefoxdev <80651379+bluefoxdev@users.noreply.github.com>
2024-08-05 21:04:14 +0100

    reuploaded project
    previous commit didn't work

--- placement.gd
+++ placement.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 
-#sets the position of the node to the global mouse position every frame.
+#move with the mouse and call get_input
 func _process(delta):
 	self.position = get_global_mouse_position()
-
-#called everytime the engine detects user input.
-func _input(event):
-	if event.is_action_just_pressed("ui_left"):
+	get_input()
+	
+func get_input():
+	if Input.is_action_just_pressed("ui_left"):
 		self.rotation_degrees -= 90
-	if event.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("ui_right"):
 		self.rotation_degrees += 90
